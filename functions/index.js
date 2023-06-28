9f14219d569eb0ec46db8f873e90ada04626678c
Leon Chen <lrchen01@gmail.com>
2023-06-27 21:42:09 -0700

    preorders notifs functions

--- functions/index.js
+++ functions/index.js
@@ -8,54 +8,68 @@ admin.initializeApp({
   databaseURL: "https://dinetime-mvp-364902-default-rtdb.firebaseio.com"
 });
 
-
-
-exports.sendPreorderNotification = functions.firestore
+exports.sendPreorderCreatedNotification = functions.firestore
     .document("preorders/{documentId}")
     .onCreate(async (snapshot) => {
-    //   try {
-    //     console.log("Received");
-    //     const newValue = snapshot.data();
-    //     const restaurantRef = admin.firestore().doc(newValue.restaurant_ref);
-    //     /* eslint-disable*/
-    //     const owners = admin.firestore().collection("owners").where("restaurant_refs", "==", restaurantRef);
-    //     const tokens = [];
-    //     owners.get().then((ownerSnapshot) => {
-    //     ownerSnapshot.forEach((owner)=>{
-    //       tokens.push(owner.data().token);
-    //     });
-    //   });
-    //   /* eslint-disable*/
-    //   let message = "";
-    //   const preorderItemsRef = admin.firestore().collection("preorders").doc(snapshot.id).collection('items');
-    //   preorderItemsRef.get().then((itemSnapshot) =>{
-    //     itemSnapshot.forEach((doc) => {
-    //       // look up query
-    //       const itemRef = doc.data().menu_item_ref;
-    //       const num = doc.data().quantity;
-    //       message += num.toString() + " " + itemRef.get().item_name + "\n";
-    //     })
-    //   })
+      try {
+        const newValue = snapshot.data();
+        const restaurantRef = newValue.restaurant_ref;
 
-      // Send message using Firebase Cloud Messaging
-      // var test = snapshot.data()
-      // var custRef = test.customer_ref;
-      // console.log(typeof(custRef));
-      // var testData = await custRef.get();
-      // console.log(testData.data().token);
+        const owners = admin.firestore().collection("owners").where("restaurant_refs", "array_contains", restaurantRef);
+        const tokens = [];
+        owners.get().then((ownerSnapshot) => {
+        ownerSnapshot.forEach((owner)=>{
+          tokens.push(owner.data().token);
+        });
+      });
+
+      // let message = "";
+      // const preorderItemsRef = admin.firestore().collection("preorders").doc(snapshot.id).collection('items');
+      // preorderItemsRef.get().then((itemSnapshot) =>{
+      //   itemSnapshot.forEach((doc) => {
+      //     // look up query
+      //     const itemRef = doc.data().menu_item_ref;
+      //     const num = doc.data().quantity;
+      //     message += num.toString() + " " + itemRef.get().item_name + "\n";
+      //   })
+      // })
+
       const payload = {
         notification: {
           title: "New Preorder!",
           body: "TESTING",
         },
-        token: "cR_ci6z1RbyyvZkOTP4AuP:APA91bEJhg-EqTLdImiipjd-wpHvFXQOBDIvUAXQ-FXei2ZjWoRYU4yS1cH-4cV3gZqfk98kKlVGwstA7QfZns-jHLhRaRD8OsAgijVd_XuJC0vC72-BQApX2yCE3TcgyTTppJwXuN-u",
+        tokens: tokens,
       };
-      /* eslint-disable*/
-      const response = await admin.messaging().send(payload);
+
+      const response = await admin.messaging().sendEachForMulticast(payload)
       console.log("Message sent successfully:", response); 
       return null;
-    // } catch (error) {
-    //   console.error(error);
-    //   throw new functions.https.HttpsError("internal", "Something went wrong");
-    // }
+    } catch (error) {
+      console.error(error);
+      throw new functions.https.HttpsError("internal", "Something went wrong");
+    }
+  });
+
+  exports.sendPreorderCompletedNotification = functions.firestore
+    .document("preorders/{documentId}")
+    .onUpdate(async (change, context) => {
+      var prevData = change.before.data()
+      var currData = change.after.data()
+      if (prevData.fulfilled == false && currData.fulfilled == true) {
+        console.log("here");
+        var customerSnapshot = await currData.customer_ref.get()
+
+        const payload = {
+          notification: {
+            title: "New Preorder!",
+            body: "TESTING",
+          },
+          token: customerSnapshot.data().token,
+        };
+
+        const response = await admin.messaging().send(payload)
+        console.log("Message sent successfully:", response); 
+      }
+      return null;
   });
